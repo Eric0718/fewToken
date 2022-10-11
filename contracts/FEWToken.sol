ad61a23a20a45f9b2a6a79cc92ce586a0f85ba6b
Eric0718 <1434797171@qq.com>
2022-10-11 10:39:08 +0800

    add fewToken

---
+++ contracts/FEWToken.sol
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT LICENSE
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract FEWToken is ERC20, ERC20Burnable, Ownable {
+    using SafeMath for uint256;
+    address[] admins;
+    uint256 public supply = 2100000000e18;
+    uint256 public subscribeFee = 100000000 gwei;
+    uint256 public subscribeAmount = 100000e18; //100000e18;
+    uint256 public totalSubscribe;
+    uint256 public immutable subscribeEndTime;
+    uint8 public constant inviteeBase = 3;
+    uint8 public rewardsBase = 5;
+
+    struct invitationInfo{
+        bool    canInvite;
+        uint256 invitationNums;
+        uint256 unclaimedNums;
+        uint256 claimed_E;
+        uint256 rewards_T;
+        uint256 claimed_T;
+        uint256 subscribe_E;
+        uint256 subscribe_T;
+    }
+
+    mapping(address => invitationInfo) invitationInfos;
+
+    event ClaimedRewards(address account,uint256 amount);
+    event ClaimedToken(address account,uint256 amount);
+
+    constructor(address controller0,address controller1,address controller2) ERC20("FEWToken", "FEW") {
+        _mint(address(this), supply);
+        subscribeEndTime = block.timestamp + 50 days;
+        admins.push(controller0);
+        admins.push(controller1);
+        admins.push(controller2);
+    }
+
+    function mint(address account,uint256 amount) public onlyOwner{
+        _mint(account, amount);
+    }
+
+    function burnFrom(address account,uint256 amount) public override{
+        super.burnFrom(account,amount);
+    }
+
+    function subscribe(address inviter)public payable{
+        require(msg.sender != inviter,"users can't invite themselves!");
+        require(msg.value == subscribeFee,"value should be 0.1 Ether");
+        require(balanceOf(address(this)) >= subscribeAmount,"there is no enough token to subscribe!");
+        _transfer(address(this),msg.sender,subscribeAmount);
+        totalSubscribe += subscribeAmount;
+        invitationInfos[msg.sender].subscribe_E += msg.value;
+        invitationInfos[msg.sender].subscribe_T += subscribeAmount;
+
+        if(!invitationInfos[msg.sender].canInvite){
+            invitationInfos[msg.sender].canInvite = true;     
+        }
+
+        if(invitationInfos[inviter].canInvite){
+            invitationInfos[inviter].invitationNums += 1;
+            invitationInfos[inviter].unclaimedNums += 1;
+
+            uint256 amount = subscribeAmount.mul(rewardsBase).div(100);
+            invitationInfos[inviter].rewards_T += amount;
+        }
+    }
+
+    function claim_E() external{
+        require(invitationInfos[msg.sender].canInvite,"No claim qualifications!");
+        uint256 num = invitationInfos[msg.sender].unclaimedNums.div(inviteeBase);
+        uint256 amount = subscribeFee * num;
+
+        require(amount > 0,"It doesn't meet the conditions");
+        require(invitationInfos[msg.sender].unclaimedNums >= num.mul(inviteeBase));
+        invitationInfos[msg.sender].unclaimedNums -= num.mul(inviteeBase);
+        require(address(this).balance >= amount,"No enough rewards to claim!");
+        invitationInfos[msg.sender].claimed_E += amount;
+        payable(msg.sender).transfer(amount);
+        emit ClaimedRewards(msg.sender, amount);
+    }
+
+    function claim_T() external {
+        require(invitationInfos[msg.sender].canInvite,"No claim qualifications!");
+        require(balanceOf(address(this)) >= invitationInfos[msg.sender].rewards_T,"No enough token to claim");
+        _transfer(address(this),msg.sender,invitationInfos[msg.sender].rewards_T);
+        invitationInfos[msg.sender].claimed_T += invitationInfos[msg.sender].rewards_T;
+        emit ClaimedToken(msg.sender,invitationInfos[msg.sender].rewards_T);
+        invitationInfos[msg.sender].rewards_T = 0;
+    }
+
+    function getRewardsInfo(address account)external view returns(
+        uint256 rewards_e,uint256 claimed_e,uint256 rewards_t,uint256 claimed_t
+        ){
+        uint256 num = invitationInfos[account].unclaimedNums.div(inviteeBase);
+        return (subscribeFee * num,invitationInfos[account].claimed_E,
+        invitationInfos[account].rewards_T,invitationInfos[account].claimed_T);
+    }
+
+    function getInviteInfo(address account) external view returns(bool isInviter,
+        uint256 totalInvitationNums,uint256 subscribe_fee,uint256 subscribe_amount) {
+        return (invitationInfos[account].canInvite,invitationInfos[account].invitationNums,
+        invitationInfos[account].subscribe_E,invitationInfos[account].subscribe_T);
+    }
+
+    function setSubscribeFee(uint256 _subFee)public onlyOwner{
+        subscribeFee = _subFee;
+    }
+
+    function setSubscribeAmount(uint256 _subAmount)public onlyOwner{
+        subscribeAmount = _subAmount;
+    }
+
+    function setRewardsBase(uint8 _rewBase)public onlyOwner{
+        rewardsBase = _rewBase;
+    }
+
+    function IsInviter()public view returns(bool){
+        return invitationInfos[msg.sender].canInvite;
+    }
+
+    function withDraw()external onlyOwner{
+        require(block.timestamp >= subscribeEndTime,"subscribe not end!");
+        uint256 amount = address(this).balance;
+        for(uint i = 0;i < admins.length;i++){
+            payable(admins[i]).transfer(amount.div(3));
+        }
+    }
+
+    function withDrawToken(address account,uint256 amount)external onlyOwner{
+        require(block.timestamp >= subscribeEndTime,"subscribe not end!");
+        uint256 balance = balanceOf(address(this));
+        require(balance >= amount,"token balance not enough!");
+        _transfer(address(this),account,amount);
+    }
+}
